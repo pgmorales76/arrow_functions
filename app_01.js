89c5e506f0af2433943962201ee0dfdd653b85e2
Restyled.io <commits@restyled.io>
2023-01-06 02:13:47 +0000

    Restyled by prettier

--- app_01.js
+++ app_01.js
@@ -1,54 +1,58 @@
-'use strict';
+"use strict";
 
 // STEP 1
 // This is a standard function expression. You may also be familiar with
 // function declarations, which begin with the "function" keyword.
-const theOldWay = function(
-    course) { return `I am currently enrolled in ${course}`; };
+const theOldWay = function (course) {
+  return `I am currently enrolled in ${course}`;
+};
 
 // TODO: Uncomment the following line of code to see the output in the browser
 // console
-console.log('The old way:', theOldWay('Code 301'));
+console.log("The old way:", theOldWay("Code 301"));
 
 // STEP 2
 // We can refactor our first function to use an arrow function.
 // The word "function" is removed and an arrow is added in between the parameter
 // and the opening curly brace
-const theNewWay =
-    (course) => { return `I am currently enrolled in ${course}`; };
+const theNewWay = (course) => {
+  return `I am currently enrolled in ${course}`;
+};
 
 // TODO: Uncomment the following line of code to see the output in the browser
 // console
-console.log('The new way:', theNewWay('Code 301'));
+console.log("The new way:", theNewWay("Code 301"));
 
 // STEP 3
 // When we have one parameter, we can omit the parentheses
-const withoutParens =
-    course => { return `I am currently enrolled in ${course}`; };
+const withoutParens = (course) => {
+  return `I am currently enrolled in ${course}`;
+};
 
 // TODO: Uncomment the following line of code to see the output in the browser
 // console
-console.log('Without parens:', withoutParens('Code 301'));
+console.log("Without parens:", withoutParens("Code 301"));
 
 // STEP 4
 // If the code block contains a single line of code, we can write everything on
 // one line We no longer need the curly braces and the return is implicit
 // Without an arrow function, we need to explicitly type "return"
-const oneLiner = course => `I cam currently enrolled in ${course}`;
+const oneLiner = (course) => `I cam currently enrolled in ${course}`;
 
 // TODO: Uncomment the following line of code to see the output in the browser
 // console
-console.log('As a one-liner:', oneLiner('Code 301'));
+console.log("As a one-liner:", oneLiner("Code 301"));
 
 // STEP 5
 // What if we have multiple parameters?
 // In a function expression, they all go in the parentheses
-const add = function(num1,
-                     num2) { return `${num1} + ${num2} = ${num1 + num2}`; };
+const add = function (num1, num2) {
+  return `${num1} + ${num2} = ${num1 + num2}`;
+};
 
 // TODO: Uncomment the following line of code to see the output in the browser
 // console
-console.log('Let\'s do some math:', add(4, 5));
+console.log("Let's do some math:", add(4, 5));
 
 // STEP 6
 // When we have multiple parameters, they must be wrapped in parentheses
@@ -57,42 +61,49 @@ const addOneLiner = (num1, num2) => `${num1} + ${num2} = ${num1 + num2}`;
 
 // TODO: Uncomment the following line of code to see the output in the browser
 // console
-console.log('Add as a one-liner:', addOneLiner(4, 5));
+console.log("Add as a one-liner:", addOneLiner(4, 5));
 
 // STEP 7
 // What if we have multiple lines of code?
 // We need to use a code block
-const multiLiner = word => {
+const multiLiner = (word) => {
   word = word.toUpperCase();
   return word;
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser
 // console
-console.log('Multi-line arrow function:', multiLiner('hello'));
+console.log("Multi-line arrow function:", multiLiner("hello"));
 
 // STEP 8
 // The way an object is returned is different with an arrow function, too.
 // Here is how we return an object without arrow functions
-const oldObject = function(array) {
-  return {firstValue : array[0], secondValue : array[1], thirdValue : array[2]};
+const oldObject = function (array) {
+  return { firstValue: array[0], secondValue: array[1], thirdValue: array[2] };
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser
 // console
-console.log('Hello from the old object function',
-            oldObject([ 'hi', 'hello', 'are you there?' ]));
+console.log(
+  "Hello from the old object function",
+  oldObject(["hi", "hello", "are you there?"])
+);
 
 // STEP 9
 // With an arrow function, we need to wrap our object in parentheses
 // Otherwise, it will be interpreted as a code block
-const newObject = array =>
-    ({firstValue : array[0], secondValue : array[1], thirdValue : array[2]});
+const newObject = (array) => ({
+  firstValue: array[0],
+  secondValue: array[1],
+  thirdValue: array[2],
+});
 
 // TODO: Uncomment the following line of code to see the output in the browser
 // console
-console.log('Hello from the new object function',
-            newObject([ 'hi', 'hello', 'are you there?' ]));
+console.log(
+  "Hello from the new object function",
+  newObject(["hi", "hello", "are you there?"])
+);
 
 // STEP 10
 // Uncomment the console.log lines to view the output of each function in the
@@ -113,9 +124,9 @@ console.log(sum(1, 2, 3, 4));
 // What if we have multiple lines of code?
 // We need to use a code block
 let objectLit = () => ({
-  key1 : 'value1',
-  key2 : 'value2',
-  key3 : 'value3',
+  key1: "value1",
+  key2: "value2",
+  key3: "value3",
 });
 
 // TODO: Uncomment the following line of code to see the output in the browser
@@ -128,7 +139,7 @@ console.log(objectLit());
 let sumAndProduct = (a, b) => {
   let sum = a + b;
   let product = a * b;
-  return [ sum, product ];
+  return [sum, product];
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser
@@ -141,19 +152,19 @@ let message = (name) => `Hello, ${name}!`;
 
 // TODO: Uncomment the following line of code to see the output in the browser
 // console
-console.log(message('Allie'));
+console.log(message("Allie"));
 
 // This looks like the old way of creating an object literal w/in a constructor
 // function Arrow functions cannot be used as constructors and will throw an
 // error when called with new. They also do not have a prototype property.
-let Student = function(name, age, hometown) {
+let Student = function (name, age, hometown) {
   this.name = name;
   this.age = age;
   this.hometown = hometown;
 };
 
 // An instance of the Student constructor function
-let joe = new Student('Joe Schmoe', 100, 'Anytown, USA');
+let joe = new Student("Joe Schmoe", 100, "Anytown, USA");
 
 // TODO: Uncomment the following line of code to see the output in the browser
 // console Note that the arrow function will cause this code to break!
@@ -163,8 +174,9 @@ console.log(joe);
 // state.
 
 // This looks like a Constructor function, with some properties in dot notation
-Student.prototype.greeting =
-    function() { return `Hi, my name is ${this.name}`; };
+Student.prototype.greeting = function () {
+  return `Hi, my name is ${this.name}`;
+};
 
 // TODO: Uncomment the following line of code to see the output in the browser
 // console Note that the arrow function will cause this method to break!
@@ -173,8 +185,9 @@ console.log(joe.greeting());
 // TODO: After viewing the previous console.log(), return the code to a working
 // state.
 
-Student.courseName =
-    function() { return 'This student is enrolled in Code 301.'; };
+Student.courseName = function () {
+  return "This student is enrolled in Code 301.";
+};
 
 // TODO: Uncomment the following line of code to see the output in the browser
 // console
@@ -182,7 +195,9 @@ console.log(Student.courseName());
 
 // STEP 11
 // How do arrow functions affect constructor functions?
-Student.prototype.scope = function() { console.log(this); };
+Student.prototype.scope = function () {
+  console.log(this);
+};
 
 // TODO: Uncomment the following line of code to see the output in the browser
 // console
